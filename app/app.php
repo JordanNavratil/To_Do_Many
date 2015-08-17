885c069a153a7b48829860932920fdf241ba802b
Dillon Lloyd Lee & Ben Spenard <student@epicodus.com>
2015-08-17 15:04:09 -0700

    convert old todo app into database compatible app.

--- app/app.php
+++ app/app.php
@@ -4,21 +4,24 @@
 
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Task.php";
+    require_once __DIR__."/../src/Category.php";
 
     // starts the $_SESSION superglobal variable, which is an array of arrays
-
-    session_start();
+    // REMOVED FOR SQL.
 
     // checks to see if the SESSION variable is empty at the specified key.
     // if it is empty, creates an array at that key
 
-    if (empty($_SESSION['list_of_tasks'])) {
-        $_SESSION['list_of_tasks'] = array();
-    }
-
     // creates a new Silex\Application object
 
     $app = new Silex\Application();
+
+    //mysql stuff:
+
+    $server = 'mysql:host=localhost;dbname=to_do';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
     // makes the twig library available to the application and tells twig to look for our template
     // in the views folder
@@ -33,9 +36,15 @@
     // contains the twig template and an array that contains the task list)
 
     $app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig');
+    });
 
+    $app->get("/tasks", function() use ($app) {
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    });
 
+    $app->get("/categories", function() use ($app) {
+        return $app['twig']->render('categories.html.twig', array('categories' =>  Category::getAll()));
     });
 
     // calls the post method on the $app object and receives a URL path as its first argument,
@@ -49,7 +58,8 @@
     $app->post("/tasks", function() use ($app) {
         $task = new Task($_POST['description']);
         $task->save();
-        return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
+        // return $app['twig']->render('create_task.html.twig', array('newtask' => $task));
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
     // calls the post method on the $app object and receives a URL path as its first argument,
@@ -60,7 +70,18 @@
 
     $app->post("/delete_tasks", function() use ($app) {
         Task::deleteAll();
-        return $app['twig']->render('delete_tasks.html.twig');
+        return $app['twig']->render('index.html.twig');
+    });
+
+    $app->post("/categories", function() use ($app) {
+        $category = new Category($_POST["name"]);
+        $category->save();
+        return $app["twig"]->render("categories.html.twig", array("categories" => Category::getAll()));
+    });
+
+    $app->post("/delete_categories", function() use ($app) {
+        Category::deleteAll();
+        return $app["twig"]->render("index.html.twig");
     });
 
     return $app;
